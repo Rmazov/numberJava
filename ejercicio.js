cbcf59873aae6fa5a48ca04465284de28a38e10b
unknown <rmazov@hotmail.com>
2022-09-21 12:19:05 -0500

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+let alturaC = 10;
+let alturaM = 10.565655
+let peso = 48.554545
+console.log(typeof alturaM);
+
+console.log(alturaM.toFixed(2));
+console.log(alturaM.toPrecision(2));
+console.log(peso.toPrecision(5));
+
+
+let max_valor_JS = Number.MAX_VALUE;
+
+console.log(max_valor_JS);
+
+if(max_valor_JS === max_valor_JS+1){
+    console.log("el máximo valor que se puede obtener en Javascript + 1 es igual al máximo valor que se puede obtener en Javascript");
+} 
